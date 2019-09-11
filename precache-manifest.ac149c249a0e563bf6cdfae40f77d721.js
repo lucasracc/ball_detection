da39d5de39eb677ad9c7d72d194d219c7803a84b
Abreu <lucas.r.abreu@accenture.com>
2019-09-10 21:40:15 -0300

    first

---
+++ precache-manifest.ac149c249a0e563bf6cdfae40f77d721.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbdc9b37dd1b5353a49f",
+    "url": "/ball_detection/static/js/runtime~main.bbdc9b37.js"
+  },
+  {
+    "revision": "83bc8c203ef33ea71d1d",
+    "url": "/ball_detection/static/js/main.83bc8c20.chunk.js"
+  },
+  {
+    "revision": "184c5c2a800138e9b288",
+    "url": "/ball_detection/static/js/1.184c5c2a.chunk.js"
+  },
+  {
+    "revision": "83bc8c203ef33ea71d1d",
+    "url": "/ball_detection/static/css/main.cd9633c9.chunk.css"
+  },
+  {
+    "revision": "7e5dc3e21af309d2b47137aa72aa2610",
+    "url": "/ball_detection/index.html"
+  }
+];
